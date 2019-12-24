1c07322739403f1e6ba701d66a7a76df3693ece3
hara <hara.acharya@gmail.com>
2019-12-23 16:01:30 -0800

    debug interface menu options added

--- debugBotGui.py
+++ debugBotGui.py
@@ -1,6 +1,6 @@
 from tkinter import *
 import time
-
+from tkinter import messagebox as msgbox
 saved_username = ["You"]
 
 class ChatWindowInterface(Frame):
@@ -19,6 +19,10 @@ class ChatWindowInterface(Frame):
         file.add_command(label = "Clear off chat", command = self.clear_screen)
         file.add_command(label = "Exit", command = self.exit_chat)
 
+        help = Menu(menu, tearoff = 0)
+        menu.add_cascade(label = "Help", menu = help)
+        help.add_command(label = "About DebugBot", command = self.about_msg)
+
 
     def clear_screen(self):
         self.text_box.config(state=NORMAL)
@@ -29,6 +33,9 @@ class ChatWindowInterface(Frame):
 
     def exit_chat(self):
         exit()
+
+    def about_msg(self):
+        msgbox.showinfo("DebugBot v1.0.0",'DebugBot is a chatbot to help debug chromeOS DUT issues. \nIt is based on retrival-based NLP.\nGUI is based on Tkinter.\nIt can answer questions regarding chromeOs build env issues.\nIt can find out issues in remote system based on system ip with asyncssh module.')
 
 
 
